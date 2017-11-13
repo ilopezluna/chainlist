0351d1461d29defb3ba1d0d46448af1afb3da9e3
ilopezluna <ignasi.lopez.luna@gmail.com>
2017-11-13 19:01:05 +0100

    A user can sell an article

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -7,10 +7,6 @@ contract ChainList {
     string description;
     uint256 price;
 
-    function ChainList() {
-        sellArticle("Default article", "This is an article set by default", 1000000000000000000);
-    }
-
     function sellArticle(string _name, string _description, uint256 _price) public {
 
         seller = msg.sender;
